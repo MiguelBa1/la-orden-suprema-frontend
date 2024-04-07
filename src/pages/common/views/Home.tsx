7a5a69bd588843cab1d1a2fc38d4b70ee3e84326
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-04-07 15:33:40 -0500

    fix: buiild error

--- src/pages/common/views/Home.tsx
+++ src/pages/common/views/Home.tsx
@@ -1,14 +1,10 @@
-import { SubmitHandler, useForm, Controller, FieldValues } from 'react-hook-form'
-import { Button, Dropdown, InputField, Table } from '@components/index'
+import { Button, Table } from '@components/index'
 import { useLogout } from '@lib/react-query-auth'
-import { categoryOptions, taskColumns, tasksData } from '@pages/common/data'
+import { taskColumns, tasksData } from '@pages/common/data'
 import { FormExample } from '@pages/common/components'
 
 export default function Home() {
-  const { control, handleSubmit, formState: { errors } } = useForm()
   const { mutate: logout } = useLogout()
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data)
 
   return (
     <div className="p-10 space-y-4">
