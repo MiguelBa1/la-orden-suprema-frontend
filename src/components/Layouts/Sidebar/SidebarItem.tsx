83375fe0747c41eb38733e5da90b67a414394bd0
Alejandro A <aalvarezbo@unal.edu.co>
2024-07-30 21:50:15 -0500

    feat: modify icons in menu items

--- src/components/Layouts/Sidebar/SidebarItem.tsx
+++ src/components/Layouts/Sidebar/SidebarItem.tsx
@@ -1,6 +1,5 @@
-import { useState, MouseEvent } from 'react'
+import React, { useState, MouseEvent } from 'react'
 import { NavLink } from 'react-router-dom'
-import { ReactNode } from 'react'
 
 type SubItemProps = {
   name: string;
@@ -9,7 +8,7 @@ type SubItemProps = {
 
 type SidebarItemProps = {
   href?: string;
-  icon: ReactNode;
+  Icon: React.ExoticComponent;
   label: string;
   isOpen: boolean;
   setIsOpen?: (value: boolean) => void;
@@ -18,7 +17,7 @@ type SidebarItemProps = {
   subItems?: SubItemProps[];
 };
 
-export function SidebarItem({ href, icon, label, isOpen, setIsOpen, subMenuOpen, setSubMenuOpen, subItems }: SidebarItemProps) {
+export function SidebarItem({ href, Icon, label, isOpen, setIsOpen, subMenuOpen, setSubMenuOpen, subItems }: SidebarItemProps) {
   const [localSubMenuOpen, setLocalSubMenuOpen] = useState(false)
 
   if (!subMenuOpen && localSubMenuOpen) {
@@ -41,7 +40,9 @@ export function SidebarItem({ href, icon, label, isOpen, setIsOpen, subMenuOpen,
       { subItems && subItems.length > 0 ? (
         <div onClick={ toggleSubMenu } className={ `cursor-pointer p-4 flex justify-center
          gap-2 hover:bg-gray-700 transition-colors` }>
-          { icon }
+          <div className="size-8">
+            <Icon/>
+          </div>
           <span className={ isOpen ? 'inline' : 'hidden' }>{ label }</span>
           <span className={ isOpen ? 'inline' : 'hidden' }>{ localSubMenuOpen ? '▲' : '▼' }</span>
         </div>
@@ -50,7 +51,9 @@ export function SidebarItem({ href, icon, label, isOpen, setIsOpen, subMenuOpen,
           to={ href || '/' }
           className={ ({ isActive }) => `cursor-pointer p-4 flex justify-center
            ${isActive ? 'bg-gray-700' : ''} gap-2 hover:bg-gray-700 transition-colors` }>
-          { icon }
+          <div className="size-8">
+            <Icon/>
+          </div>
           { isOpen && <span>{ label }</span> }
         </NavLink>
       ) }
