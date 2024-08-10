3ee75963668bc878d51e554db9220f133ae69d24
Alejandro A <aalvarezbo@unal.edu.co>
2024-08-10 18:48:43 -0500

    feat: add image field and confirm modal to create new assassin view

--- src/pages/admin/assassins/components/CreateAssassinForm.tsx
+++ src/pages/admin/assassins/components/CreateAssassinForm.tsx
@@ -1,18 +1,52 @@
-import { useForm, Controller } from 'react-hook-form'
+import { useForm, Controller, FieldValues, SubmitHandler, SubmitErrorHandler } from 'react-hook-form'
 import { countriesList } from '@data/index'
 import { InputField, Dropdown, Button } from '@components/index'
+import { AssassinPhoto, CreateAssassinConfirmModal } from '@pages/admin'
+import { useState, useRef } from 'react'
+import { useToastStore } from '@stores/useToastStore.ts'
+import { useNavigate } from 'react-router-dom'
 
 export function CreateAssassinForm() {
+  const methods = useForm<FieldValues>()
+  const formRef = useRef<HTMLFormElement | null>(null)
+  const { addToast } = useToastStore()
+  const navigate = useNavigate()
+
   const {
     register,
     control,
     handleSubmit,
     formState: { errors }
-  } = useForm()
+  } = methods
+
+  const [photoUrl, setPhotoUrl] = useState<string>()
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false)
+
+  const onSubmit: SubmitHandler<FieldValues> = () => {
+    navigate('/app/admin/assassins')
+    addToast({
+      type: 'success',
+      message: 'Asesino registrado correctamente'
+    })
+  }
+
+  const onInvalid: SubmitErrorHandler<FieldValues> = () => {
+    addToast({
+      type: 'error',
+      message: 'Por favor, completa todos los campos'
+    })
+  }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4" >
-      <form className="lg:col-span-2 space-y-4" onSubmit={ handleSubmit(() => console.log("Envía información")) }>
+      <AssassinPhoto
+        photoUrl={ photoUrl }
+        onPhotoUpdated={ (newPhotoUrl) => setPhotoUrl(newPhotoUrl) }
+        isDisabled={ false }
+        methods={ methods }
+        required
+      />
+      <form className="lg:col-span-2 space-y-4" ref={ formRef } onSubmit={ handleSubmit(onSubmit, onInvalid) }>
         <div className="grid sm:grid-cols-2 gap-4">
           <InputField
             id="name"
@@ -71,12 +105,22 @@ export function CreateAssassinForm() {
                 message: 'El email no es válido'
               }
             }) }
+            error={ errors.email?.message as string }
           />
         </div>
         <div className="flex justify-center lg:justify-end">
-          <Button type="submit">Registrar asesino</Button>
+          <Button onClick={ () => setIsConfirmModalOpen(true) }>Registrar asesino</Button>
         </div>
       </form>
+
+      <CreateAssassinConfirmModal
+        isOpen={ isConfirmModalOpen }
+        onClose={ () => setIsConfirmModalOpen(false) }
+        onConfirm={ () => {
+          setIsConfirmModalOpen(false)
+          formRef.current?.requestSubmit()
+        } }
+      />
     </div>
   )
 }
