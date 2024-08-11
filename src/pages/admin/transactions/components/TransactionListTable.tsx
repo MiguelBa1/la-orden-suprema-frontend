b87cfb1906213cee479981802d11a396a6607ef9
Juanjo <jtobonz@unal.edu.co>
2024-08-11 15:15:14 -0500

    fix: update TransactionListTable

--- src/pages/admin/transactions/components/TransactionListTable.tsx
+++ src/pages/admin/transactions/components/TransactionListTable.tsx
@@ -2,35 +2,35 @@ import { UseQueryResult } from '@tanstack/react-query'
 import { Table, Spinner } from '@components/UI'
 import { TransactionListMock, TransactionTableColumns, type TransactionList } from '@pages/admin'
 
-type MissionsTableProps = {
-  missionsListQuery: UseQueryResult<TransactionList>;
+type TransactionsTableProps = {
+  transactionListQuery: UseQueryResult<TransactionList>;
 }
 
-export function MissionsListTable({ missionsListQuery }: MissionsTableProps) {
+export function TransactionListTable({ transactionListQuery }: TransactionsTableProps) {
 
-  if (missionsListQuery.isFetching) {
+  if (transactionListQuery.isFetching) {
     return <div className="flex justify-center items-center h-96">
       <Spinner />
     </div>
   }
 
-  if (missionsListQuery.isError) {
+  if (transactionListQuery.isError) {
     return <div className="flex justify-center items-center h-96">
       Error obteniendo la lista de misiones
     </div>
   }
 
-  if (missionsListQuery.data?.length === 0) {
+  if (transactionListQuery.data?.length === 0) {
     return <div className="flex justify-center items-center h-96">
       No se encontraron resultados
     </div>
   }
 
-  if (!missionsListQuery.data) {
+  if (!transactionListQuery.data) {
     return null
   }
 
   return (
-    <Table columns={ TransactionTableColumns } data={ TransactionListMock } />
+    <Table columns={ TransactionTableColumns } data={ transactionListQuery } />
   )
 }
