20888d575d3d3c72f83f710cd1daf3662985b7f5
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-08-18 18:28:50 -0500

    feat: add AssassinsListView

---
+++ src/pages/assassin/assassins/views/AssassinsListView.tsx
@@ -0,0 +1,12 @@
+export function AssassinsListView() {
+
+  return (
+    <div className="space-y-6">
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl lg:text-2xl">
+        Asesinos
+        </h1>
+      </div>
+    </div>
+  )
+}
