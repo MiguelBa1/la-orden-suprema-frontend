ffbcef497e79eaf388c1d6a0779df6a2290f0a09
Miguel Barrera <miguelbarreraa05@gmail.com>
2024-08-24 15:31:22 -0500

    feat: add BuyAssassinsDetailsModal

---
+++ src/pages/assassin/assassins/components/BuyAssassinsDetailsModal.tsx
@@ -0,0 +1,52 @@
+import { Modal, Button } from '@components/index'
+import { AssassinTableRow } from '@pages/assassin'
+import { useToastStore } from '@stores/index'
+
+type BuyAssassinsDetailsModalProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  assassin: AssassinTableRow | null;
+}
+
+export function BuyAssassinsDetailsModal({ isOpen, onClose, assassin }: BuyAssassinsDetailsModalProps) {
+  const { addToast } = useToastStore()
+
+  const handleBuyAssassin = () => {
+    // TODO: Implement buy assassin logic
+
+    addToast({
+      message: '¡Compra exitosa!',
+      type: 'success',
+    })
+
+    onClose()
+  }
+
+  return (
+    <Modal
+      isOpen={ isOpen }
+      onClose={ onClose }
+      title="Confirmación"
+      footerButtons={
+        <>
+          <Button onClick={ onClose } variant="secondary">Cancelar</Button>
+          <Button
+            variant="primary"
+            color="green"
+            onClick={ handleBuyAssassin }
+          >
+            Comprar
+          </Button>
+        </>
+      }
+    >
+      <p>
+        ¿Desea comprar la información de { assassin?.name }?
+      </p>
+      <p>
+        Esto le permitirá ver: Nombre completo, dirección y foto.
+        Esta acción tiene un costo de { assassin?.price } monedas de asesino.
+      </p>
+    </Modal>
+  )
+}
