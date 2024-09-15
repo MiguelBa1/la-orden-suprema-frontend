6b397f6b524e7aa4f179db4182c75220173928c5
miguel <miguelbarreraa05@gmail.com>
2024-09-14 20:51:28 -0500

    feat: add signs in transactions table

--- src/pages/admin/transactions/data/TransactionListColumns.tsx
+++ src/pages/admin/transactions/data/TransactionListColumns.tsx
@@ -5,7 +5,9 @@ import { TransactionItem } from '@pages/admin/transactions'
 export const TransactionTableColumns: Column<TransactionItem>[] = [
   { title: 'Descripción', dataIndex: 'description', key: 'description' },
   { title: 'Movimiento', dataIndex: 'type', key: 'transaction' },
-  { title: 'Cantidad', dataIndex: 'amount', key: 'amount' },
+  { title: 'Cantidad', dataIndex: 'amount', key: 'amount', render: record => {
+    return record.type === 'Entrada' ? `+${record.amount}` : `-${record.amount}`
+  } },
   { title: 'Fecha', dataIndex: 'date', key: 'date', render: record => {
     return dayjs(record.date).format('DD/MM/YYYY HH:mm')
   } }
