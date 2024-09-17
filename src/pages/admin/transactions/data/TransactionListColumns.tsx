622e9484a8ebc27e127326e191001a767cb88d12
Alejandro A <aalvarezbo@unal.edu.co>
2024-09-17 00:08:09 -0500

    feat: update create mission

--- src/pages/admin/transactions/data/TransactionListColumns.tsx
+++ src/pages/admin/transactions/data/TransactionListColumns.tsx
@@ -5,9 +5,7 @@ import { TransactionItem } from '@pages/admin/transactions'
 export const TransactionTableColumns: Column<TransactionItem>[] = [
   { title: 'Descripción', dataIndex: 'description', key: 'description' },
   { title: 'Movimiento', dataIndex: 'type', key: 'transaction' },
-  { title: 'Cantidad', dataIndex: 'amount', key: 'amount', render: record => {
-    return record.type === 'Entrada' ? `+${record.amount}` : `-${record.amount}`
-  } },
+  { title: 'Cantidad', dataIndex: 'amount', key: 'amount' },
   { title: 'Fecha', dataIndex: 'date', key: 'date', render: record => {
     return dayjs(record.date).format('DD/MM/YYYY HH:mm')
   } }
